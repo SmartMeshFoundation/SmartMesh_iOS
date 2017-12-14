ebc519f207da15f752a90f31c46334edf0a50e31
Rain Dou <634778311@qq.com>
2017-12-14 18:28:16 +0800

    Add Some Models
    Add Some Models

---
+++ SmartMesh/SmartMesh/ApplicationClasses/Contacts/Model/FFContactsRequestItem.h
@@ -0,0 +1,36 @@
+//
+//  FFContactsRequestItem.h
+//  NextApp
+//
+//  Created by Megan on  17-12-12.
+//  Copyright (c) 2017年 SmartMesh Foundation All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFContactsRequestItem : NSObject
+
+//(timeStamp INTEGER, chatType INTEGER, messageSendState INTEGER, messageReadState INTEGER, uidFrom TEXT, nickName TEXT, messageID TEXT, remoteID TEXT, groupName TEXT, content TEXT, accepted TEXT default '0', messageType INTEGER, PRIMARY KEY(messageID))
+//
+//(timeStamp, chatType, messageSendState, messageReadState, uidFrom, nickName, messageID, remoteID, content, accepted, messageType)
+//
+//@property (nonatomic, assign) NSInteger timeStamp;
+//@property (nonatomic, copy) NSString *userImage;
+//@property (nonatomic, copy) NSString *userImage;
+//@property (nonatomic, copy) NSString *userImage;
+//@property (nonatomic, copy) NSString *userImage;
+//@property (nonatomic, copy) NSString *userImage;
+//@property (nonatomic, copy) NSString *userImage;
+//@property (nonatomic, copy) NSString *userImage;
+//
+//
+//
+//@property (nonatomic, copy) NSString *userImage;
+//@property (nonatomic, copy) NSString *nickName;
+//@property (nonatomic, copy) NSString *textContent; // 好友的留言
+//@property (nonatomic, copy) NSString *uidFrom; // localid
+//
+//
+//@property (nonatomic, copy) NSString *accepted; // 已经添加
+
+@end
---
+++ SmartMesh/SmartMesh/ApplicationClasses/Contacts/Model/FFContactsRequestItem.m
@@ -0,0 +1,13 @@
+//
+//  FFContactsRequestItem.m
+//  NextApp
+//
+//  Created by Megan on  17-12-12.
+//  Copyright (c) 2017年 SmartMesh Foundation All rights reserved.
+//
+
+#import "FFContactsRequestItem.h"
+
+@implementation FFContactsRequestItem
+
+@end
